a350fe20528cd73f7dbf1c84d69165b1d1146302
abdelrahman <abood.wael123@gmail.com>
2020-05-04 21:19:27 +0400

    Population move in a random way

--- socialDistancing.py
+++ socialDistancing.py
@@ -16,7 +16,9 @@ SCREEN_TITLE = "Social Distancing the Game"
 
 # Choose the staring population size
 POPULATION = 20
-POPULATION_SPEED = 10
+POPULATION_SPEED = 7
+BOUNCINESS = 1
+CIRCLE_RADIUS = 8
 
 class MyGame(arcade.Window):
     """
@@ -42,7 +44,9 @@ class MyGame(arcade.Window):
         # Create your sprites and sprite lists here
         
         # Setting the spawn coordinates for the population and their initial velocities
-        for x in range(0,POPULATION):     
+        for x in range(0,POPULATION):    
+            change_x = 1
+            change_y = -1 
             # Setting the initial velocity of the population
             delta_x = random.randint(-POPULATION_SPEED,POPULATION_SPEED)
             if delta_x >= 0:
@@ -57,7 +61,7 @@ class MyGame(arcade.Window):
                 spawn_coord_x = random.randint(10, SCREEN_WIDTH - 10)
                 spawn_coord_y = random.randint(10, SCREEN_HEIGHT - 10)
 
-            self.people.append([spawn_coord_x, spawn_coord_y, delta_x, delta_y])
+            self.people.append([spawn_coord_x, spawn_coord_y, delta_x, delta_y, change_x, change_y])
         pass
 
     def on_draw(self):
@@ -73,7 +77,7 @@ class MyGame(arcade.Window):
         arcade.draw_circle_outline(self.player[0]/2, self.player[1]/2, 30, arcade.color.GREEN, 2)
         
         for person in self.people:
-            arcade.draw_circle_filled(person[0], person[1], 8, arcade.color.BLACK)
+            arcade.draw_circle_filled(person[0], person[1], CIRCLE_RADIUS, arcade.color.BLACK)
 
     def on_update(self, delta_time):
         """
@@ -83,17 +87,41 @@ class MyGame(arcade.Window):
         """
 
         for person in self.people:
-            coord_x = person[0]
-            coord_y = person[1]
-            delta_x = person[2]
-            delta_y = person[3] 
-
             # Update the x and y coordinates based on the velocity
-            coord_x += delta_x 
-            coord_y += delta_y
+            person[0] += person[2]
+            person[1] += person[3]
 
-            person[0] = coord_x
-            person[1] = coord_y
+            # If the population hits either of the side edges it bounces back
+            if person[0] < CIRCLE_RADIUS and person[2] < 0:
+                person[2] *= -BOUNCINESS
+
+            elif person[0] > SCREEN_WIDTH - CIRCLE_RADIUS and person[2] > 0:
+                person[2] *= -BOUNCINESS
+
+            # If it hit the edges at the bottom or top
+            if person[1] < CIRCLE_RADIUS and person[3] < 0:
+                person[3] *= -BOUNCINESS
+
+            elif person[1] > SCREEN_HEIGHT - CIRCLE_RADIUS and person[3] > 0:
+                person[3] *= -BOUNCINESS
+
+            # Change Velocity of person by chance
+            chance = random.randint(0,100)
+            if chance <= 10:
+                if person[2] == POPULATION_SPEED:
+                    person[4] = -1
+
+                if person[2] == -POPULATION_SPEED:
+                    person[4] = 1
+
+                if person[3] == POPULATION_SPEED:
+                    person[5] = -1
+
+                if person[3] == -POPULATION_SPEED:
+                    person[5] = 1
+
+                person[2] += person[4]
+                person[3] += person[5]
         pass
         
 
